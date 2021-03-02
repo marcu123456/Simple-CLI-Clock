aee5067c507cb9905aacf4de4fc77a543ed4cdf8
Marcus <mdb013@protonmail.com>
2021-03-03 09:58:39 +1100

    ?

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-mv cli-clock.py ~/.cli-clock.py
-sudo chmod +x cli-clock
-sudo mv cli-clock /usr/local/bin
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+    mv cli-clock.py ~/.cli-clock.py
+    sudo chmod +x cli-clock
+    sudo mv cli-clock /usr/local/bin
