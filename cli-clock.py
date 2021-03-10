246e635ab7a01146f1f19a9ea2719ebc44c53e0b
Marcus <mdb013@protonmail.com>
2021-03-10 16:46:39 +1100

    Seperated Windows & UNIX into seperate functions.

--- cli-clock.py
+++ cli-clock.py
@@ -3,25 +3,39 @@ from time import sleep
 import os
 import sys
 
-def clear():
-    os.system('clear')
+def windows():
+    while True:
+        try:
+            now = datetime.now()
+            timeCurrent = now.strftime("%H:%M:%S")
 
-# Windows check. So the thing doesn't brake.
+            print(timeCurrent)
+
+            sleep(1)
+            os.system('cls')
+        except KeyboardInterrupt:
+            sys.exit(0)
+
+def unix():
+    while True:
+        try:
+            now = datetime.now()
+            timeCurrent = now.strftime("%H:%M:%S")
+
+            print(timeCurrent)
+
+            sleep(1)
+            os.system('clear')
+        except KeyboardInterrupt:
+            sys.exit(0)
+
 if os.name == 'nt':
-    print('Simple CLI-Clock does not support Windows.')
-    sys.exit(1)
+    windows()
+else:
+    unix()
+    
 
-while True:
-    try:
-        now = datetime.now()
-        timeCurrent = now.strftime("%H:%M:%S")
 
-        print(timeCurrent)
-
-        sleep(1)
-        clear()
-    except KeyboardInterrupt:
-        sys.exit(0)
 
     
 
